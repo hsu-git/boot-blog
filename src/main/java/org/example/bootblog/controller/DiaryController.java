4e42911c5af059aa2bdbdacdd5b1c9c986d6233a
hsu-git <hsu080206@gmail.com>
2025-04-14 13:55:07 +0900

    .

--- src/main/java/org/example/bootblog/controller/DiaryController.java
+++ src/main/java/org/example/bootblog/controller/DiaryController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller // Spring context
 @RequestMapping("/diary")
 public class DiaryController {
     private final DiaryService diaryService;
--- src/main/java/org/example/bootblog/controller/IndexController.java
+++ src/main/java/org/example/bootblog/controller/IndexController.java
@@ -5,7 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller // Spring Context
+// @RequestMapping("/")    // MVC
 public class IndexController {
     @GetMapping
     public String index(Model model) {
